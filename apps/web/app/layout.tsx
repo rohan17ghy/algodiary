a561c3112e18f6dc3024766b061b691b75004c6f
Debopriya_Bhattachar <rohan17ghy@gmail.com>
2024-11-12 17:19:23 +0530

    Added recoil for state management
    Added recoil for state management

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { RecoilRoot } from "recoil";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -24,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <RecoilRoot>{children}</RecoilRoot>
       </body>
     </html>
   );
