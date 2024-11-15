caded3ede630d57ff0d05c01e3b86a74101516fd
Debopriya_Bhattachar <rohan17ghy@gmail.com>
2024-11-15 08:03:18 +0530

    Update trades state
    Update trades state

--- packages/store/src/atoms/trades.ts
+++ packages/store/src/atoms/trades.ts
@@ -4,3 +4,13 @@ export const showImportTradesAtom = atom({
   key: "showImportTrades",
   default: false,
 });
+
+export const currentPageAtom = atom({
+  key: "currentPage",
+  default: 1,
+});
+
+export const tradesPerPageAtom = atom({
+  key: "tradesPerPage",
+  default: 5,
+});
