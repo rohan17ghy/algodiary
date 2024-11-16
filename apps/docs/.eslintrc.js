a70cc67ec00c91ec519f41363458dc953a9104e8
Debopriya_Bhattachar <rohan17ghy@gmail.com>
2024-11-16 12:02:38 +0530

    Removed the default docs app
    Removed the default docs app

--- apps/docs/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@algodiary/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
